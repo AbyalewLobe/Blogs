e9bb12319f9bd8ad7eea846ec4191db8240b9ecc
Abyalew <labyalew@gmail.com>
2024-10-16 14:22:50 +0300

    second commit

--- app.js
+++ app.js
@@ -7,27 +7,32 @@
 
 //register view engine
 app.set('view engine', 'ejs');
-
+const blogs =[
+   {title: 'first blog', snippet: 'agfdaghahfgagagdagf'},
+   {title: 'second blog', snippet: 'oioioioioioioioioiii'},
+   {title: 'third blog', snippet: 'fdfsfsfsdsfsfsfsfsfsf'},
+];
  app.get('/',(req,res)=>{
-  res.render('index');
+   
+  res.render('index',{title : 'home', blogs});
  })
  app.get('/about',(req,res)=>{
-   res.render('about');
+   res.render('about',{title : 'home', blogs});
  })
 
  app.get('/contact',(req,res)=>{
-    res.render('contact');
+    res.render('contact',{title : 'home', blogs});
  })
  app.get('/catagories',(req,res)=>{
-    res.render('catagories');
+    res.render('catagories',{title : 'home', blogs});
  })
 
  app.get('/blogs/create',(req,res)=>{
-    res.render('craete');
+    res.render('craete',{title : 'home', blogs});
  })
  app.get('/explore',(req,res)=>{
     res.render('explore');
  })
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.status(404).sendFile('./views/404.ejs',{root:__dirname});
 })
