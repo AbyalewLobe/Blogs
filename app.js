90de7316ad2b42c309998b7d3cdf435097402832
Abyalew <labyalew@gmail.com>
2024-10-17 06:12:19 -0700

    fifth commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
  const morgan = require('morgan');
  const mongoose = require('mongoose');
  const Blog = require('./models/bolg');
+ const multer = require('multer');
 
  
  const app = express()
@@ -19,13 +20,22 @@ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
- app.use(morgan('dev'));
 
- app.use(express.static('public'));
+
+
 
 //register view engine
 app.set('view engine', 'ejs');
 
+//middleware & static files
+app.use((req,res,next)=>{
+   next();
+});
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(morgan('dev'));
 
 
  app.get('/',(req,res)=>{
@@ -33,7 +43,7 @@ app.set('view engine', 'ejs');
   res.redirect('/blogs');
  })
  app.get('/about',(req,res)=>{
-   res.render('about',{title : 'home', blogs});
+   res.render('about',{title : 'home'});
  })
 
  //blog routs
@@ -45,20 +55,49 @@ app.set('view engine', 'ejs');
       .catch((err)=>{
          console.log(err);
       })
- })
+ });
+
+ app.post('/craete',(req,res)=>{
+   const blog = new Blog(req.body);
+
+   blog.save()
+      .then((result)=>{
+         res.redirect('/blogs');
+      })
+      .catch((err)=>{
+         console.log(err);
+      })
+ });
+
+ app.get('/blogs/:id', (req, res) => {
+   const id = req.params.id;
+   Blog.findById(id)
+       .then(result => {
+           if (result) {
+               res.render('details', { blog: result, title: 'Blog Details' });
+           } else {
+               res.status(404).render('404', { title: 'Blog Not Found' });
+           }
+       })
+       .catch(err => {
+           console.log(err);
+           res.status(500).render('404', { title: 'Server Error' });
+       });
+});
+
 
  app.get('/contact',(req,res)=>{
-    res.render('contact',{title : 'home', blogs});
+    res.render('contact',{title : 'contact'});
  })
  app.get('/catagories',(req,res)=>{
-    res.render('catagories',{title : 'home', blogs});
+    res.render('catagories',{title : 'catagories'});
  })
 
  app.get('/craete',(req,res)=>{
-    res.render('craete',{title : 'home', blogs});
+    res.render('craete',{title : 'create'});
  })
  app.get('/explore',(req,res)=>{
-    res.render('explore');
+    res.render('explore',{title : 'explore'});
  })
 app.use((req,res)=>{
     res.status(404).sendFile('./views/404.ejs',{root:__dirname});
